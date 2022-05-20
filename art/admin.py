084db3f62332c51c32b7db6dbe898403605250eb
Inotila <inoati1991@gmail.com>
2022-05-20 19:06:11 +0000

    update admin.py to add a comment panale

--- art/admin.py
+++ art/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,4 +7,14 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
     
     prepopulated_fields = {'slug':('title',)}
+    list_filter = ('status',)
+    search_fields = ['title', 'completed_on']
+    list_display = ('title', 'status', 'completed_on')
     # summernote_fields = ('title')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_on')
+    search_fields = ['name', 'email', 'created_on']
+    list_filter = ('created_on',)
