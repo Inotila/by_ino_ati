bccf5a73694f90cdea8316eea8f96d4680d438a8
Inotila <inoati1991@gmail.com>
2022-05-22 14:59:42 +0000

    add code for view to render and templates for html pages

--- art/models.py
+++ art/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0,"Draft"), (1,"Published"))
 
 class Post(models.Model):
--- art/views.py
+++ art/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostGird(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('completed_on')
+    template_name = 'index.html'
