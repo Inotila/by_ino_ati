e5ba4c561769422af4fbeed7ac78e3f52a992237
Inotila <inoati1991@gmail.com>
2022-05-22 18:14:59 +0000

    update views.py and add links to correct pages in nav found in the base.html

--- art/urls.py
+++ art/urls.py
@@ -5,3 +5,11 @@ from django.urls import path
 urlpatterns = [ 
     path('', views.HomePage.as_view(), name='home')
 ]
+
+urlpatterns = [ 
+    path('', views.HomePage.as_view(), name='home'),
+    path('templates/paintings.html', views.PaintingPage.as_view(), name='painting'),
+    path('templates/ink.html', views.InkPage.as_view(), name='ink'),
+    path('templates/pencil.html', views.PencilPage.as_view(), name='pencil')
+
+]
--- art/views.py
+++ art/views.py
@@ -5,3 +5,18 @@ from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+class PaintingPage(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-completed_on')
+    template_name = 'paintings.html'
+
+class InkPage(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-completed_on')
+    template_name = 'ink.html'
+
+class PencilPage(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-completed_on')
+    template_name = 'pencil.html'
