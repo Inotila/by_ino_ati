801d6b36621d2d190aebfeb0838bb587dbdede80
Inotila <inoati1991@gmail.com>
2022-05-22 21:02:35 +0000

    add allauth for user signups and add path to view in url.py

--- byinoati/urls.py
+++ byinoati/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('summernote/', include('django_summernote.urls')),
     path('', include('art.urls'), name='byinoati_urls'),
+    path('accounts/',include('allauth.urls')),
 ]
