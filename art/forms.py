da212fad42d6d562e21abe22daa56a497891a02f
Inotila <inoati1991@gmail.com>
2022-05-24 14:36:16 +0000

    update forms.py fields to body so it's reconginized

--- art/forms.py
+++ art/forms.py
@@ -1,8 +1,8 @@
 from .models import Comment
-from .django import forms
+from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('user_comments',)
+        fields = ('body',)
