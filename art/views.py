5443939da6da889a2cb811417f112afe4eb6cdca
Inotila <inoati1991@gmail.com>
2022-05-24 17:50:46 +0000

    import http response to reload template

--- art/views.py
+++ art/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from django.views.generic import TemplateView
 from .forms import CommentForm
