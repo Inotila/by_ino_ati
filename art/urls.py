29c63fb568667e820369b573e9995afbb79000fd
Inotila <inoati1991@gmail.com>
2022-05-24 13:28:12 +0000

    uncomment art_details so details.html page can view. And update details.html template

--- art/urls.py
+++ art/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('templates/paintings.html', views.PaintingPage.as_view(), name='painting'),
     path('templates/ink.html', views.InkPage.as_view(), name='ink'),
-    path('templates/pencil.html', views.PencilPage.as_view(), name='pencil')
-    # path('<slug:slug>/', views.ArtDetails.as_view(),name='art_details'),
+    path('templates/pencil.html', views.PencilPage.as_view(), name='pencil'),
+    path('<slug:slug>/', views.ArtDetails.as_view(),name='art_details'),
 ]
--- art/views.py
+++ art/views.py
@@ -28,7 +28,7 @@ class ArtDetails(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.order_by('created_on')
         liked = False
-        if posst.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
