2df26ff52b59cab1e45392724fbf4d418b4a6a84
Inotila <inoati1991@gmail.com>
2022-05-24 19:55:24 +0000

    add message tags for bootstrap color alerts

--- byinoati/settings.py
+++ byinoati/settings.py
@@ -59,6 +59,14 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+MESSAGES_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
