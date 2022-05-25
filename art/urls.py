2c28c5f366e4a1c1564d90c2b45a691b3e69d648
Inotila <inoati1991@gmail.com>
2022-05-25 08:52:28 +0000

    update url path for home to be more specific

--- art/urls.py
+++ art/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [ 
-    path('', views.HomePage.as_view(), name='home'),
+    path('templates/index.html', views.HomePage.as_view(), name='home'),
     path('templates/paintings.html', views.PaintingPage.as_view(), name='painting'),
     path('templates/ink.html', views.InkPage.as_view(), name='ink'),
     path('templates/pencil.html', views.PencilPage.as_view(), name='pencil'),
