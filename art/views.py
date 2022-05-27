cd5e4124a834fa1e5e74f70e0762d1282974d82f
Inotila <inoati1991@gmail.com>
2022-05-27 16:50:45 +0000

    add a blank link to edit page

--- art/views.py
+++ art/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post
 from django.views.generic import TemplateView
+from .models import Post
 from .forms import CommentForm
 
 
