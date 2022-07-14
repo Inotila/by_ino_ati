0131d23b4369822084f545369c51f9ac8ffa30d3
Inotila <inoati1991@gmail.com>
2022-07-14 18:01:31 +0000

    Added a false debug status to the settings to protect sensitive info

--- byinoati/settings.py
+++ byinoati/settings.py
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['by-ino-ati.herokuapp.com', 'localhost']
 
